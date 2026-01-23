f86e9c3e4516216001d1e4e776e20e6773a3bee6
Sergiofer1994 <sergiofer1994@gmail.com>
2026-01-23 15:57:01 +0100

    fix: modifying the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import './Header.css';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -17,7 +19,7 @@ const Header = () => {
         <header className="bg-slate-900 sticky top-0 z-50 shadow-lg">
             <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
 
-                {/* Logo + Nombre */}
+                
                 <div className="flex items-center gap-3">
                     <img
                         src="/logo.png"
@@ -25,19 +27,19 @@ const Header = () => {
                         className="w-10 h-10"
                     />
                     <h1 className="text-xl font-bold text-white tracking-widest">
-                        CRIME<span className="text-red-600">FLIX</span>
+                        Escena<span className="text-red-600">Del Crimen</span>
                     </h1>
                 </div>
 
-                {/* Botón menú móvil */}
+                
                 <button
                     className="md:hidden text-gray-300 text-2xl"
                     onClick={() => setMenuOpen(!menuOpen)}
                 >
-                    ☰
+                    
                 </button>
 
-                {/* Navegación */}
+                
                 <nav
                     className={`${menuOpen ? "block" : "hidden"
                         } absolute top-full left-0 w-full bg-slate-900 md:static md:block md:w-auto`}
